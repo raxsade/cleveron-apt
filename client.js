664b436fe33cc48b87331be0adf9a47cf66099db
Ragnar Säde <raxsade@gmail.com>
2015-07-21 16:54:41 +0300

    refactor

--- client.js
+++ client.js
@@ -1,12 +1,14 @@
 var net = require('net');
-var client = net.connect({port: 1337, host: '46.101.163.47'}, function() { //'connect' listener
-	client.write('sade');
+var API_KEY = 'sade';
+var client = net.connect({port: 1337, host: '46.101.163.47'});
+client.on('connect', function () {
+	client.write(API_KEY);
 	client.write('pull');
-});
-client.on('data', function(data) {
+})
+client.on('data', function (data) {
 	console.log(data.toString());
 	// client.end();
 });
-client.on('end', function() {
+client.on('end', function () {
 	console.log('disconnected from server');
 });
