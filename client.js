e7ca2a1bb26910c7a68a0a7f6728183374ae430e
Ragnar Säde <raxsade@gmail.com>
2015-07-23 14:32:51 +0300

    refactor and add comments

--- client.js
+++ client.js
@@ -1,47 +1,72 @@
 var net = require('net');
 var _ = require('underscore');
+
+// possible prefixes for responses by the server
+var responsePrefixes = ['S: {', 'E: {', 'C: {'];
+// the current APT data
+var currentData;
+
 var API_KEY = 'sade';
 var client = net.connect({port: 1337, host: '46.101.163.47'});
+
 client.on('connect', function () {
 	client.write(API_KEY);
+	// pull command
 	client.write('pull');
-})
-client.on('data', function (data) {
-	handleResponses(data.toString());
+});
+client.on('data', function (chunk) {
+	handleResponses(chunk.toString());
 });
 client.on('end', function () {
 	console.log('disconnected from server');
 });
 
-var responsePrefixes = ['S: {', 'E: {', 'C: {'];
-var currentData;
+/**
+	Handles responses.
+	Since server buffer may be smaller than response size,
+	creates a queue of response chunks and when response
+	is complete, handles it according to its prefix.
+*/
 function handleResponsesClosure () {
+
+	// create closure to remember the response chunks queue
 	var chunks = '';
+
 	return function (newChunk) {
+
 		console.log(newChunk);
+
 		var startsWithPrefix = responsePrefixes.indexOf(newChunk.slice(0,4)) > -1;
+		// if new response chunk starts with responsePrefix then discard the old chunks queue
 		chunks = startsWithPrefix ? newChunk : chunks + newChunk;
+
 		try {
 			// response prefix 'E' throws an error
 			if ('E' === chunks[0]) {
 				throw(chunks);
 			};
-			var jsonObject = JSON.parse(chunks.slice(3));
+
+			// try to parse chunks, catch error if fails
+			// if succeeds (response is complete) then handle according to prefix
+			var responseObject = JSON.parse(chunks.slice(3));
 
 			// response prefix 'S' as a response to "pull" command
 			if ('S' === chunks[0]) {
-				currentData = jsonObject;
+				currentData = responseObject;
 			};
 
 			// response prefix 'C' - changed data
 			if ('C' === chunks[0]) {
-				var objectToChange = _.find(currentData[jsonObject.collection], function (obj) {
-					return obj.id === jsonObject.id;
+				// find the object to change in the currentData
+				var objectToChange = _.find(currentData[responseObject.collection], function (obj) {
+					return obj.id === responseObject.id;
 				});
-				_.extend(objectToChange, jsonObject.change);
+				// overwrite changed properties
+				_.extend(objectToChange, responseObject.change);
 			};
 
 			console.log(currentData);
+			
 		} catch(e) {
 			console.log(e);
 		}
